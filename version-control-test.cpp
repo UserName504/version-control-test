e4ca4f596702d2714ce51e10cf97a7c5901b8b7d
UserName504 <dszalai@hotmail.co.uk>
2022-04-18 09:23:09 +0100

    Made the date hardcoded.

--- version-control-test.cpp
+++ version-control-test.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
-#include <chrono>
-#include <ctime> 
+//#include <chrono>
+//#include <ctime> 
 
 using namespace std;
 
 int main()
 {
-	auto right_now = chrono::system_clock::now();
-    time_t output = chrono::system_clock::to_time_t(right_now);
-    cout << "Compiled on: " << ctime(&output);
+	//auto right_now = chrono::system_clock::now();
+    //time_t output = chrono::system_clock::to_time_t(right_now);
+    //cout << "Compiled on: " << ctime(&output);
+    cout << "Compiled on:\nMonday, April 18th, 2022." << endl;
 	return 0;
 }
