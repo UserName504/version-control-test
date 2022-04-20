f0307680c2f0e3a522145e693a968cd829c98b18
UserName504 <dszalai@hotmail.co.uk>
2022-04-20 20:30:42 +0100

    Removed a few comments.

--- version-control-test.cpp
+++ version-control-test.cpp
@@ -1,14 +1,8 @@
 #include <iostream>
-//#include <chrono>
-//#include <ctime> 
 
-//using namespace std;
+using namespace std;
 
 int main()
 {
-	//auto right_now = chrono::system_clock::now();
-    //time_t output = chrono::system_clock::to_time_t(right_now);
-    //cout << "Compiled on: " << ctime(&output);
-    std::cout << "Wednesday, April 20th, 2022." << std::endl; //<< endl;
-	//return 0;
+    cout << "Wednesday, April 20th, 2022." << endl;
 }
