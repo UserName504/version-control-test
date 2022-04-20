f6181b836056c82c8d123412e5a7f163a9145014
UserName504 <dszalai@hotmail.co.uk>
2022-04-20 17:04:23 +0100

    Wednesday, April 20th, 2022.

--- version-control-test.cpp
+++ version-control-test.cpp
@@ -9,6 +9,6 @@ int main()
 	//auto right_now = chrono::system_clock::now();
     //time_t output = chrono::system_clock::to_time_t(right_now);
     //cout << "Compiled on: " << ctime(&output);
-    cout << "Compiled on:\nMonday, April 18th, 2022." << endl;
+    cout << "Wednesday, April 20th, 2022." << endl;
 	return 0;
 }
