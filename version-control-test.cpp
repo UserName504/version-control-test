8775b42f4bdafd97f162f9458fc8364571dac3d0
UserName504 <dszalai@hotmail.co.uk>
2022-09-26 13:17:52 +0100

    More Fork testing on Monday, September 26th.

--- version-control-test.cpp
+++ version-control-test.cpp
@@ -5,6 +5,7 @@ using namespace std;
 #define MAGENTA "\033[35m"
 
 int main() {
+    cout << "Another edit on:" << endl;
     cout << MAGENTA << "Monday, September 26th, 2022." << endl;
     return 0;
 }
